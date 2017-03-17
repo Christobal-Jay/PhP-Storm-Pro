3baa6ba02b8ba51b18390681a8703224587a520b
Christobal-Jay <christobal.amj@gmail.com>
2017-03-17 22:54:35 +0100

    Atec
    Test1

---
+++ atec/typo3conf/ext/ot_bootstrap3/Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,8 @@
+<?php
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+    'ot_bootstrap3',
+    'Configuration/PageTS/PageTSConfigFile.t3s',
+    'Website configuration'
+
+    );
